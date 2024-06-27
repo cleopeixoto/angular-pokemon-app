effe9cc124129fd28f437b92bba11c6b416c5c1d
cleopeixoto <32248265+cleopeixoto@users.noreply.github.com>
2024-06-26 22:46:28 -0300

    chore(main): removed unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 import { CardService } from './services/card.service';
 
 @Component({
